50da22ec05cea75763b963a451aeb73584e3ef6f
alexHooper <alexmohack@gmail.com>
2018-09-13 21:15:49 +0530

    unzip the mdbootstrap-gulp package

---
+++ MDB-Gulp-Free_4.5.10/js/modules.js
@@ -0,0 +1,12 @@
+exports.modules = [
+  './js/_intro-mdb-free.js',
+  './js/vendor/jquery.easing.js',
+  './js/vendor/velocity.min.js',
+  './js/vendor/chart.js',
+  './js/vendor/wow.js',
+  './js/dist/scrolling-navbar.js',
+  './js/vendor/waves.js',
+  './js/dist/forms-free.js',
+  './js/vendor/enhanced-modals.js'
+  // './js/vendor/addons/datatables.js'
+];
